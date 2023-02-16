a882486e02aafd479a205b0afd8c30f466de60d4
Mutshinye <Mutshinyen@gmail.com>
2023-02-16 03:31:50 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,10 +3,11 @@
 
 /**
  *main - C program that prints exactly
- *Return: 1
+ *Return: Always 1
  */
 
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
