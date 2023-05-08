cd44ecd7d30086f00822450fd0a3e9b73561493c
Mutshinye <Mutshinyen@gmail.com>
2023-05-08 13:14:57 -0700

    3-cp.c

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,116 +1,114 @@
 #include "main.h"
 #include <stdio.h>
 
-int close_file(int fd);
-void print_read_error(char *filename);
-void print_write_error(char *filename);
+int close_on_error(int file_descriptor);
+void read_on_error(char *file_name);
+void write_on_error(char *file_name);
 
 /**
- * main - Copies the contents of one file to another.
- * @argc: Number of arguments passed to the program.
- * @argv: Array of arguments passed to the program.
- * Return: Always returns 0, or exits with an error code.
+ *main - copies the content of a file to another file.
+ *@argument_counter: argument counter.
+ *@argument_vector: argument vector.
+ *Return: always return 0, or exit.
  */
-int main(int argc, char *argv[])
+int main(int argument_counter, char *argument_vector[])
 {
-	ssize_t bytes_read = 1;
+	ssize_t counted_bytes = 1;
 	char buffer[1024];
-	int fd_from, fd_to;
+	int file_from, file_to;
 
-	/* Check number of arguments */
-	if (argc != 3)
+	/*check number of argument */
+	if (argument_counter != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	/* Open and check file_from */
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from < 0)
+	/*open and check file_from */
+	file_from = open(argument_vector[1], O_RDONLY);
+	if (file_from < 0)
 	{
-		print_read_error(argv[1]);
+		read_on_error(argument_vector[1]);
 		exit(98);
 	}
 
-	/* Open and check file_to */
-	fd_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	if (fd_to < 0)
+	/*open and check  file_to */
+	file_to = open(argument_vector[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	if (file_to < 0)
 	{
-		print_write_error(argv[2]);
-		close_file(fd_from);
+		write_on_error(argument_vector[2]);
+		close_on_error(file_from);
 		exit(99);
 	}
 
-	/* Copy contents */
-	while (bytes_read)
+	/*copy content - loop checks end of file (file_from) */
+	while (counted_bytes)
 	{
-		/* Read the next 1024 bytes from fd_from and check for read errors */
-		bytes_read = read(fd_from, buffer, 1024);
-		if (bytes_read < 0)
+		/*read the next 1020 bytes in file_from and check read error */
+		counted_bytes = read(file_from, buffer, 1024);
+		if (counted_bytes < 0)
 		{
-			print_read_error(argv[1]);
-			close_file(fd_from);
-			close_file(fd_to);
+			read_on_error(argument_vector[1]);
+			close_on_error(file_from);
+			close_on_error(file_to);
 			exit(98);
 		}
 
-		/* Check for end of file */
-		if (bytes_read == 0)
+		/*check buffer end of file */
+		if (counted_bytes == 0)
 			break;
-
-		/* Write the contents to fd_to and check for write errors */
-		bytes_read = write(fd_to, buffer, bytes_read);
-		if (bytes_read < 0)
+		/*write the output in file_to and check write error */
+		counted_bytes = write(file_to, buffer, counted_bytes);
+		if (counted_bytes < 0)
 		{
-			print_write_error(argv[2]);
-			close_file(fd_from);
-			close_file(fd_to);
+			write_on_error(argument_vector[2]);
+			close_on_error(file_from);
+			close_on_error(file_to);
 			exit(99);
 		}
 	}
 
-	/* Close files */
-	if (close_file(fd_from) < 0)
+	/*close and check  file_from and file_to */
+	if (close_on_error(file_from) < 0)
 	{
-		close_file(fd_to);
+		close_on_error(file_to);
 		exit(100);
 	}
 
-	if (close_file(fd_to) < 0)
+	if (close_on_error(file_to) < 0)
 		exit(100);
 	return (0);
 }
 
 /**
- * close_file - Closes a file descriptor and checks for errors.
- * @fd: File descriptor to be closed.
- * Return: 1 if file descriptor could be closed, -1 if it could not.
+ *close_on_error - close a file descriptor and check for a possible error.
+ *@file_descriptor: file descriptor for file to be closed.
+ *Return: 1 if file_descriptor colud be closeed, -1 if file_descriptor could not be closed.
  */
-int close_file(int fd)
+int close_on_error(int file_descriptor)
 {
-	int output;
+	int close_output;
 
-	output = close(fd);
-	if (output < 0)
-		dprintf(STDERR_FILENO, "Error: Can't close file descriptor %d\n", fd);
-	return (output);
+	close_output = close(file_descriptor);
+	if (close_output < 0)
+		dprintf(STDERR_FILENO, "Error: Can't close file_descriptor %d\n", file_descriptor);
+	return (close_output);
 }
 
 /**
- * print_read_error - Prints the read error message.
- * @filename: Name of the file that caused the error.
+ *read_on_error - print the read error.
+ *@file_name: file_name.
  */
-void print_read_error(char *filename)
+void read_on_error(char *file_name)
 {
-	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_name);
 }
 
 /**
- * print_write_error - Prints the write error message.
- * @filename: Name of the file that caused the error.
+ *write_on_error - print the write error.
+ *@file_name: file_name.
  */
-void print_write_error(char *filename)
+void write_on_error(char *file_name)
 {
-	dprintf(STDERR_FILENO, "Error: Can't write to file %s\n", filename);
+	dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_name);
 }
-
